da1d8362758874aaa1e14d7e964f5c73ecafc73b
paulleerentveld <paul@veldnet.com.au>
2022-05-21 15:30:42 +1000

    lab done

---
+++ src/Messages.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Messages extends React.Component {
+    
+    render() { 
+        return (
+            <div>
+                <h1>This is my Messages component!</h1>
+                <ul>
+                    <li>Hello this is the first message!</li>
+                    <li>Hello this is the Second message!</li>
+                    <li>Hello this is the Third message!</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default Messages ;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Home from './Home';
+import About from './Home';
+import Login from './Login';
+import Messages from './Messages';
+import SignUp from './SignUp';
+import Navbar from './Navbar';
 
-const Home = () => {
-  return (
+
+
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/messages" component={Messages} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
+
